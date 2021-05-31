7f63dd5dc1f69aac36a7b76edba616df2f2657c3
Jose Luis GR <A01063645@itesm.mx>
2021-05-30 19:17:36 -0500

    branch detail

--- api/main.py
+++ api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
-from git_ops import get_all_branches
+from git_ops import (
+  get_all_branches,
+  get_branch_detail
+)
 
 app = FastAPI()
 
@@ -12,6 +15,14 @@ def home():
 def get_branches():
   status_code = status.HTTP_204_NO_CONTENT
   result = get_all_branches()
+  if result:
+    status_code = status.HTTP_200_OK
+  return JSONResponse(status_code=status_code, content=result)
+
+@app.get("/api/v1/branches/{branch}", tags=["Git"])
+def get_branch_commits(branch: str):
+  status_code = status.HTTP_204_NO_CONTENT
+  result = get_branch_detail(branch)
   if result:
     status_code = status.HTTP_200_OK
   return JSONResponse(status_code=status_code, content=result)
